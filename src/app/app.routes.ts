fab65f7036c25a500c9f21715d914f827bdfcf4d
Fernando Facchi <nandojosefacchi2017@gmail.com>
2024-11-02 21:28:25 -0300

    Criando componentes para tela de login e cadastro, juntamente da biblioteca "Bootstrap"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CadastrarComponent } from './home/cadastrar/cadastrar.component';
+import { LoginComponent } from './home/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent,
+    children: [
+      { path: 'cadastrar', component: CadastrarComponent },
+      { path: 'login', component: LoginComponent},
+      // { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  },
+];
